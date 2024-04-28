4b3b1ce37a794c8f888f0f98c0e2c535ef3a5317
Gejsi <vgejsi@gmail.com>
2024-04-29 00:14:40 +0200

    Refactor numbers lexing

--- src/lexer/literal.rs
+++ src/lexer/literal.rs
@@ -1,3 +1,5 @@
+use std::{fmt::Display, num::ParseIntError};
+
 use nom::{
     branch::alt,
     bytes::complete::{escaped, tag},
@@ -7,6 +9,7 @@ use nom::{
     sequence::{pair, preceded, separated_pair, terminated, tuple},
     IResult,
 };
+use thiserror::Error;
 
 use super::LexerError;
 
@@ -17,15 +20,45 @@ pub enum Literal<'a> {
     Number(Number),
 }
 
-// TODO: questo è veramente temporaneo, non so neanche come vogliamo chiamare i nostri tipi
-// (vogliamo la nomenclatura alla java?).
-// Vanno ancora distinti gli interi decimali da binari e compagnia.
+#[derive(Debug, PartialEq, PartialOrd, Clone)]
+pub enum BitCount {
+    _8,
+    _16,
+    _32,
+    _64,
+}
+
+impl Display for BitCount {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let n = match self {
+            BitCount::_8 => 8,
+            BitCount::_16 => 16,
+            BitCount::_32 => 32,
+            BitCount::_64 => 64,
+        };
+        write!(f, "{}", n)
+    }
+}
+
 #[derive(Debug, PartialEq, PartialOrd)]
-pub enum Number {
-    Integer(i32),
-    Long(i64),
-    Float(f32),
-    Double(f64),
+pub enum NumberValue {
+    Integer(u64),
+    Floating(f64),
+}
+
+#[derive(Debug, PartialEq, PartialOrd)]
+pub struct NumberKind(char, BitCount);
+
+impl Display for NumberKind {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}{}", self.0, self.1)
+    }
+}
+
+#[derive(Debug, PartialEq, PartialOrd)]
+pub struct Number {
+    pub kind: Option<NumberKind>,
+    pub value: NumberValue,
 }
 
 fn lex_bool(i: &str) -> IResult<&str, bool, LexerError> {
@@ -42,86 +75,97 @@ fn lex_string(i: &str) -> IResult<&str, &str, LexerError> {
     )(i)
 }
 
-fn lex_integer(i: &str) -> IResult<&str, i64, LexerError> {
-    map_res(
+fn remove_number_sugar(i: &str) -> String {
+    i.replace("_", "")
+}
+
+fn lex_decimal(i: &str) -> IResult<&str, String, LexerError> {
+    map(
         recognize(pair(digit1, many0(alt((digit1, tag("_")))))),
-        |s: &str| s.replace("_", "").parse::<i64>(),
+        remove_number_sugar,
     )(i)
 }
 
-// TODO: togliere gli underscore, ho provato ad usare `map` per togliere gli underscore ma `replace` ritorna `String` e a
-// noi serve `&str`.
-fn decimal(i: &str) -> IResult<&str, &str, LexerError> {
-    recognize(pair(digit1, many0(alt((digit1, tag("_"))))))(i)
+fn lex_float(i: &str) -> IResult<&str, String, LexerError> {
+    map(
+        separated_pair(lex_decimal, char('.'), cut(lex_decimal)),
+        |(int_part, frac_part)| remove_number_sugar(&format!("{int_part}.{frac_part}")),
+    )(i)
+}
+
+fn lex_bit_count(i: &str) -> IResult<&str, BitCount, LexerError> {
+    alt((
+        value(BitCount::_8, char('8')),
+        value(BitCount::_16, tag("16")),
+        value(BitCount::_32, tag("32")),
+        value(BitCount::_64, tag("64")),
+    ))(i)
+}
+
+fn lex_number_type(i: &str) -> IResult<&str, NumberKind, LexerError> {
+    map(pair(alt((char('f'), char('u'), char('i'))), lex_bit_count), |(c,b)| NumberKind(c,b))(i)
+}
+
+fn lex_exponent(i: &str) -> IResult<&str, i64, LexerError> {
+    map_res(
+        tuple((one_of("eE"), opt(one_of("+-")), cut(lex_decimal))),
+        |(_, sign, decimal)| {
+            let n = format!("{}{}", sign.unwrap_or('+'), remove_number_sugar(&decimal));
+            n.parse::<i64>()
+        },
+    )(i)
+}
+
+// TODO: proper number error handling pls
+#[derive(Error, Debug)]
+enum NumberParsingError {
+    #[error("")]
+    LexError,
+
+    #[error("Failed to parse to a 32 bit integer: {0}")]
+    ParseIntError(#[from] ParseIntError),
+
+    #[error("Failed to parse {0} which contains exponent")]
+    IntegerWithExponent(NumberKind),
+}
+
+fn parse_number(
+    value: String,
+    exp: Option<i64>,
+    kind: Option<NumberKind>,
+) -> Result<Number, NumberParsingError> {
+    let final_kind = match (exp, kind) {
+        (Some(_), None) => Ok(Some(('f', BitCount::_64))),
+        (Some(_), Some(NumberKind('f', size))) => Ok(Some(('f', size))),
+        (Some(_), Some(NumberKind('i', _))) | (Some(_), Some(NumberKind('u', _))) => {
+            return Err(NumberParsingError::LexError)
+        }
+        (_, kind) => Ok(kind),
+    }?;
+    let has_dot = value.contains('.');
+    let parsed_value = match (final_kind.clone(), has_dot) {
+        // Parse as a float if it's hard-coded or if the number contains a dot
+        (Some(('f', _)), _) | (_, true) => value
+            .parse::<f64>()
+            .map(|f| NumberValue::Floating(exp.unwrap_or(1) as f64 * f))?,
+        _ => value.parse::<u64>().map(NumberValue::Integer)?,
+    }?;
+
+    Ok(Number {
+        kind: final_kind,
+        value: parsed_value,
+    })
 }
 
 fn lex_number(i: &str) -> IResult<&str, Number, LexerError> {
-    alt((
-        // TODO: handle hexadecimal floats (vogliamo questa cosa?)
-        // NOTE: this section handle floats
-        // Case: .42
-        // recognize(tuple((
-        //     char('.'),
-        //     decimal,
-        //     opt(tuple((one_of("eE"), opt(one_of("+-")), cut(decimal)))),
-        // ))),
-        // Case: 42e42 and 42.42e42
-        // recognize(tuple((
-        //     decimal,
-        //     opt(preceded(char('.'), decimal)),
-        //     one_of("eE"),
-        //     opt(one_of("+-")),
-        //     cut(decimal),
-        // ))),
-        // Case: 42.42
-        map_res(
-            tuple((
-                decimal,
-                char('.'),
-                decimal,
-                opt(alt((tag("f32"), tag("f64")))),
-            )),
-            |(int_part, _, frac_part, suffix)| {
-                let float = format!("{int_part}.{frac_part}");
-
-                Ok::<Number, LexerError>(match suffix {
-                    Some(suffix) => {
-                        if suffix == "f64" {
-                            Number::Double(float.parse::<f64>().unwrap_or(0.0))
-                        } else {
-                            Number::Float(float.parse::<f32>().unwrap_or(0.0_f32))
-                        }
-                    }
-                    None => Number::Double(float.parse::<f64>().unwrap_or(0.0)),
-                })
-            },
-        ),
-        // Case: 42.
-        // recognize(tuple((decimal, char('.')))),
-
-        // TODO: handle binary/octal/hexadecimal integers
-        // NOTE: this section handle integers
-        map_res(
-            // Ho provato ad usare `cut` qui per short-circuitare il parsing quando si sbaglia a
-            // scrivere i suffissi ma non andava come volevo, es. `10i3` dovrebbe fallire ma ora
-            // lascia semplicemente `i3` come resto dell'input e fa solo il parsing di `10`.
-            // Poi qui servono anche tutti gli altri suffissi...
-            tuple((decimal, opt(alt((tag("i32"), tag("i64")))))),
-            |(s, suffix)| {
-                // non mi piace sto codice, gli `unwrap_or` :(
-                Ok::<Number, LexerError>(match suffix {
-                    Some(suffix) => {
-                        if suffix == "i64" {
-                            Number::Long(s.parse::<i64>().unwrap_or(0))
-                        } else {
-                            Number::Integer(s.parse::<i32>().unwrap_or(0))
-                        }
-                    }
-                    None => Number::Integer(s.parse::<i32>().unwrap_or(0)),
-                })
-            },
-        ),
-    ))(i)
+    map_res(
+        tuple((
+            alt((lex_float, lex_decimal)),
+            opt(lex_exponent),
+            opt(lex_number_type),
+        )),
+        |(value, exp, kind)| parse_number(value, exp, kind),
+    )(i)
 }
 
 pub fn lex_literal(i: &str) -> IResult<&str, Literal, LexerError> {
@@ -134,7 +178,11 @@ pub fn lex_literal(i: &str) -> IResult<&str, Literal, LexerError> {
 
 #[cfg(test)]
 mod tests {
-    use crate::lexer::{lex_literal, literal::Number, Literal};
+    use crate::lexer::{
+        lex_literal,
+        literal::{BitCount, Number, NumberValue},
+        Literal,
+    };
 
     #[test]
     fn match_bool() {
@@ -142,68 +190,93 @@ mod tests {
         assert_eq!(lex_literal("false"), Ok(("", Literal::Bool(false))));
     }
 
-    #[test]
-    fn match_long() {
-        assert_eq!(
-            lex_literal("123i64"),
-            Ok(("", Literal::Number(Number::Long(123))))
-        );
+    macro_rules! assert_number_expr {
+        ($n: expr, $kind: expr, $value: expr) => {
+            assert_eq!(
+                lex_literal($n),
+                Ok((
+                    "",
+                    Literal::Number(Number {
+                        kind: $kind,
+                        value: $value
+                    })
+                ))
+            );
+        };
     }
 
     #[test]
-    fn match_double() {
-        assert_eq!(
-            lex_literal("123.1"),
-            Ok(("", Literal::Number(Number::Double(123.1))))
+    fn match_u64() {
+        assert_number_expr!(
+            "123u64",
+            Some(('u', BitCount::_64)),
+            NumberValue::Integer(123)
         );
-        assert_eq!(
-            lex_literal("123.1f64"),
-            Ok(("", Literal::Number(Number::Double(123.1))))
-        );
-        assert_eq!(
-            lex_literal("123.10f32"),
-            Ok(("", Literal::Number(Number::Float(123.1))))
+
+        // Without suffix
+        assert_number_expr!("123", None, NumberValue::Integer(123));
+    }
+
+    #[test]
+    fn match_i64() {
+        assert_number_expr!(
+            "123u64",
+            Some(('i', BitCount::_64)),
+            NumberValue::Integer(123)
         );
     }
 
-    // #[test]
-    // fn temp_test() {
-    // assert_eq!(
-    //     lex_literal(".456_e-12"),
-    //     Ok(("", Literal::Number(Number::Long(123))))
-    // );
-    // assert_eq!(
-    //     lex_literal("21.456e+12"),
-    //     Ok(("", Literal::Number(Number::Long(123))))
-    // );
-    // assert_eq!(
-    //     lex_literal("21.100_10"),
-    //     Ok(("", Literal::Number(Number::Long(123))))
-    // );
-    // assert_eq!(
-    //     lex_literal("21."),
-    //     Ok(("", Literal::Number(Number::Long(123))))
-    // );
-    // assert_eq!(
-    //     lex_literal("21"),
-    //     Ok(("", Literal::Number(Number::Long(123))))
-    // );
-    // }
+    fn match_f64() {
+        assert_number_expr!(
+            "123f64",
+            Some(('f', BitCount::_32)),
+            NumberValue::Floating(123f64)
+        );
+        assert_number_expr!(
+            "123.23f64",
+            Some(('f', BitCount::_64)),
+            NumberValue::Floating(123.23f64)
+        );
+        assert_number_expr!(
+            "123.23e10f64",
+            Some(('f', BitCount::_64)),
+            NumberValue::Floating(123.23e10f64)
+        );
+        assert_number_expr!(
+            "123.23E10f64",
+            Some(('f', BitCount::_64)),
+            NumberValue::Floating(123.23E10f64)
+        );
 
-    // #[test]
-    // fn match_int_underscore() {
-    //     assert_eq!(lex_literal("1_000"), Ok(("", Literal::Integer(1000))));
-    // }
+        // Without suffix
+        assert_number_expr!("123.0", None, NumberValue::Floating(123.0));
+        assert_number_expr!("123.23", None, NumberValue::Floating(123.23));
+        assert_number_expr!("123.23e10", None, NumberValue::Floating(123.23e10));
+        assert_number_expr!("123.23E10", None, NumberValue::Floating(123.23E10));
+    }
 
-    // #[test]
-    // fn match_double() {
-    //     assert_eq!(lex_literal("1.23"), Ok(("", Literal::Double(1.23))));
-    // }
-
-    // #[test]
-    // fn match_double_with_multiple_decimals() {
-    //     assert_eq!(lex_literal("1.1.1"), Err(NErr::Error(("", ErrorKind::Tag))));
-    // }
+    fn match_f32() {
+        assert_number_expr!(
+            "123f32",
+            Some(('f', BitCount::_32)),
+            NumberValue::Floating(123.0)
+        );
+        assert_number_expr!(
+            "123.23f32",
+            Some(('f', BitCount::_32)),
+            NumberValue::Floating(123.23)
+        );
+        assert_number_expr!(
+            "123.23e10f32",
+            Some(('f', BitCount::_32)),
+            NumberValue::Floating(123.23e10)
+        );
+        assert_number_expr!(
+            "123.23E10f32",
+            Some(('f', BitCount::_32)),
+            NumberValue::Floating(123.23E10)
+        );
+    }
 
     #[test]
     fn match_simple_string() {
